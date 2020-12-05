02d591a46d1223bf16da69c6efa960602198ab69
luochao <327412817@qq.com>
2020-12-05 05:21:30 -0800

    在类中增加一个统计数据方法，并实现这个方法的功能

--- src/cn/mldn/LinkDemo.java
+++ src/cn/mldn/LinkDemo.java
@@ -1,6 +1,7 @@
 package cn.mldn;
 interface ILink<E> { //设置泛型避免安全隐患
 	public void add(E e) ;
+	public int size() ;
 
 }
 class LinkImpl<E> implements ILink<E> {
@@ -14,11 +15,16 @@ class LinkImpl<E> implements ILink<E> {
 		//第二次调用：this = LinkImpl.root.next ;
 		//第三次调用：this = LinkImpl.root.next.next ;
 		public void addNode(Node newNode) {	//保存新的Node数据
-			
+			if(this.next == null) {	//当前节点的下一个节点为null
+				this.next = newNode ;	//保存当前节点
+			}else {	//
+				this.next.addNode(newNode);
+			}
 		}
 	}
 	//---------------以下为Link类中定义的成员---------------
-	private Node root ;
+	private Node root ;	//保存根元素
+	private int count ; //保存数据的个数
 	//---------------以下为Link类中定义的方法---------------
 	public void add(E e) {
 		if(e == null) {	//保存的数据为null
@@ -31,10 +37,19 @@ class LinkImpl<E> implements ILink<E> {
 		}else {	//根节点存在
 			this.root.addNode(newNode); //将新节点保存在合适的位置
 		}
+		this.count ++ ;	//统计数据个数
+	}
+	public int size() {
+		return this.count ;	//将统计后数据返回
 	}
 }
 public class LinkDemo {
 	public static void main(String args[]) {
-		System.out.println("Hello");
+		ILink<String> all = new LinkImpl<String> () ;
+		System.out.println("【增加之前】数据个数：" + all.size());
+		all.add("Hello");
+		all.add("World");
+		all.add("Mldn");
+		System.out.println("【增加之后】数据个数：" + all.size());
 	}
 }
